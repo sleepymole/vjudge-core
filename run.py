899b9ce157674d5469fdcf83ee692339782deebe
Yujie Xia <xiayjchn@gmail.com>
2018-07-15 02:27:23 +0800

    update requirements.txt

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ import subprocess
 
 from vjudge.main import VJudge
 
-subprocess.Popen(shlex.split("gunicorn -w 2 -k gevent -b 'localhost:5000' manage:app"))
+p = subprocess.Popen(shlex.split("gunicorn -w 2 -k gevent -b 'localhost:5000' manage:app"))
 
-vjudge = VJudge()
-
-vjudge.start()
+try:
+    vjudge = VJudge()
+    vjudge.start()
+finally:
+    p.terminate()
+    p.wait()
