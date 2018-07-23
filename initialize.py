87e3374b001341fa017cfaa7b51df726da905301
Yujie Xia <xiayjchn@gmail.com>
2018-07-23 23:44:16 +0800

    update crawler

--- initialize.py
+++ initialize.py
@@ -1,30 +1,30 @@
-import os
 import logging
 from datetime import datetime
 
-from vjudge.site import exceptions, HDUClient, SOJClient
+from vjudge.site import exceptions, get_normal_client
 from vjudge.models import db, Problem
 
 logging.basicConfig(level=logging.INFO)
 
 
-def init_db():
-    db.create_all()
-
-
-def crawler(oj_name, client, start, end):
-    for i in range(start, end):
-        problem_id = str(i)
+# TODO: remove crawler in the future
+def crawler(site):
+    client = get_normal_client(site)
+    try:
+        problem_list = client.get_problem_list()
+    except exceptions.ConnectionError:
+        return
+    for problem_id in problem_list:
         try:
             result = client.get_problem(problem_id)
         except exceptions.ConnectionError:
             continue
         if result:
-            p = Problem.query.filter_by(oj_name=oj_name, problem_id=problem_id).first() or Problem()
+            p = Problem.query.filter_by(oj_name=site, problem_id=problem_id).first() or Problem()
             for attr in result:
                 if hasattr(p, attr):
                     setattr(p, attr, result[attr])
-            p.oj_name = oj_name
+            p.oj_name = site
             p.problem_id = problem_id
             p.last_update = datetime.utcnow()
             db.session.add(p)
@@ -33,9 +33,8 @@ def crawler(oj_name, client, start, end):
 
 
 def main():
-    init_db()
-    crawler('scu', SOJClient(), 1000, 5000)
-    crawler('hdu', HDUClient(), 1000, 7000)
+    crawler('scu')
+    crawler('hdu')
 
 
 if __name__ == '__main__':
